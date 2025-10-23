4e9f243e78d36e2ae598d7e429eb311adb320b83
Binxu Wang <binxu_wang@hms.harvard.edu>
2025-10-23 17:02:07 -0400

    update the nonnegative spectrum clamping

--- taap_utils_torch.py
+++ taap_utils_torch.py
@@ -86,6 +86,8 @@ def constructGram(F, field):
 
 def reconstructFrame(G, m, n, field):
     positive_spectrum, Q = torch.linalg.eigh(G)
+    # ensure the positive spectrum is non-negative
+    positive_spectrum = positive_spectrum.clamp(min=0)
     if field == "real":
         F = torch.diag(torch.sqrt(positive_spectrum[n-m:])) @ Q[:,n-m:].T
     elif field == "complex":
